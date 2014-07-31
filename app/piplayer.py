623a7f464250958be5331464d3c02e26ea6cc966
Adrian Lange <alange@thebrighttag.com>
2014-07-30 22:15:12 -0500

    initial login impl

--- app/piplayer.py
+++ app/piplayer.py
@@ -1,19 +1,35 @@
 from flask import Flask
+from flask import request
+from flask import url_for
+from flask import redirect
+from flask import session
+
 app = Flask(__name__)
 
-tmp_html = """
-<html>
-  <audio controls>
-    <source src="/static/music/Tiga/Ciao! + bonus remixes/09 Shoes.mp3" type="audio/mpeg">
-  </audio>
-</html>
-"""
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('static', filename='index.html'))
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
 
 @app.route('/')
-def hello_world():
-    return tmp_html
+def index():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    return redirect(url_for('static', filename='index.html'))
 
 
 if __name__ == '__main__':
     #app.debug = False
+    #app.debug = True
+
+    app.secret_key = 'banana'
     app.run()
