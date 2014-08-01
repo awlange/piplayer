4287fb624acde4edae16adf4260c06bee0e6f49c
Adrian Lange <alange@thebrighttag.com>
2014-08-01 18:41:55 -0500

    added password

--- app/piplayer.py
+++ app/piplayer.py
@@ -8,19 +8,19 @@ from flask import render_template
 import argparse
 
 app = Flask(__name__)
-valid_usernames = set()
+valid_usernames = {}
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """
     Handle logging in using session
-    Verifies that the posted username is in the whitelist
+    Verifies that the posted username is in the whitelist and has the correct password
     """
     if request.method == 'POST':
         req_username = request.form['username']
         print valid_usernames
-        if req_username in valid_usernames:
+        if req_username in valid_usernames and valid_usernames.get(req_username) == request.form['password']:
             session['username'] = req_username
             return redirect(url_for('index'))
         return render_template('login.html', invalid_username=True)
@@ -45,11 +45,13 @@ def setup():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('secret_key', help='secret key for sessions')
-    parser.add_argument('usernames', help='CSV set of valid user names, e.g. "bob,sally,jennifer"')
+    parser.add_argument('usernames_passwords', help='CSV set of valid user names colon separated with their passwords, '
+                                                    'e.g. "bob:password,sally:luvsalad11,jennifer:n3v3rgu3$$"')
     args = parser.parse_args()
     app.secret_key = args.secret_key
-    for username in args.usernames.split(','):
-        valid_usernames.add(username)
+    for username_password in args.usernames_passwords.split(','):
+        username, _, password = username_password.partition(':')
+        valid_usernames[username] = password
 
 
 if __name__ == '__main__':
