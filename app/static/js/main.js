aaa3890653cc6c1cfaa2774f79b8ec9d6c4db83e
Adrian Lange <alange@thebrighttag.com>
2014-08-03 16:19:36 -0500

    working on custom player, better controls

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready( function(){
     artistClick();
+    controller();
 });
 
 function artistClick() {
@@ -45,23 +46,26 @@ function albumClick() {
 }
 
 function songClick() {
-    // respond to clicking on song, play song
-    $(".song").click( function(e) {
+    // respond to clicking on song
+    var song = $(".song");
+    song.click( function(e) {
         updateCurrentlyPlaying($(this));
-        updateAudio($(this));
-    });    
+        updateAudio($(this), false);
+    });  
 }
 
 function updateCurrentlyPlaying(song) {
     var album = song.parent().parent();
     var artist = album.parent().parent();
     var artistName = getElementText(artist);
+    var albumTitle = getElementText(album);
     var songTitle = getElementText(song);
     $("#currently_playing").replaceWith("<p id=\"currently_playing\">" + 
-        artistName + " : " + songTitle + "</p>");
+        artistName + " / " + albumTitle + " / " + songTitle + "</p>");
 }
 
-function updateAudio(song) {
+function updateAudio(song, startPlay) {
+    // path is added as a hidden html element after AJAX call
     var path = song.children().first().text();
     if ($("source").length) {
         $("source").attr("src", path);
@@ -69,16 +73,39 @@ function updateAudio(song) {
         $("audio").append("<source src=\"" + path + "\" type=\"audio/mpeg\">");
     }
 
-    // play song on click
-    var audio = $("audio");
-    audio[0].pause();
-    audio[0].load();
-    audio[0].play();
+    // load song, optionally play song
+    var audio = $("audio")[0];
+    audio.pause();
+    audio.load();
+    if (startPlay) {
+        updatePlayButton();
+    }
 }
 
 function getElementText(element) {
     // Gets the text of an element sans the text of its children
-    return element.contents().filter( function(){
+    return element.contents().filter( function() {
         return this.nodeType == 3;  // text of the node
     })[0].nodeValue;
+}
+
+function controller() {
+    $("#play_button").click( function() {
+        updatePlayButton();
+    });
+}
+
+function updatePlayButton() {
+    console.log("trying to update");
+    var audio = $("audio")[0];
+    var button = $("#play_button");
+    if (audio.paused) {
+        audio.play();
+        button[0].className = "";
+        button[0].className = "pause";
+    } else {
+        audio.pause();
+        button[0].className = "";
+        button[0].className = "play"
+    } 
 }
